a3b7867420e3f4cc57d67ed16013ce0747850842
grey <grey@infinitesys.my>
2018-02-01 18:05:20 +0800

    2-1

---
+++ wp-content/themes/pencilone/base.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * The main template file
+ * @Auther pencilone
+ * @version 1.0
+ */
+
+?>
---
+++ wp-content/themes/pencilone/readme.txt
@@ -0,0 +1,4 @@
+=== cn-blogs ===
+Contributors: cn-blogs
+Version: 1.0
+License: GPLv2 or later
