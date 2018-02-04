e57b43a3ad157d7dfe1546418e4b12032b7e8ab9
root <root@pencil.(none)>
2018-02-04 22:24:40 +0800

    2-4

--- wp-content/themes/pencilone/functions.php
+++ wp-content/themes/pencilone/functions.php
@@ -10,7 +10,13 @@
 function daxiawp_sidebar(){
     register_sidebar(array(
           'id'=>'hander_sidebar_search',
-          'name'=>'头部搜索'
+          'name'=>'头部搜索',
+          'description'=>'hander search',
+          'class'=>'',
+          'before_widget'=>'<div>',
+          'after_widget'=>'</div>',
+          'before_title'=>'<h2>',
+          'after_title'=>'</h2>'
     ));
 }
 add_action('widgets_init','daxiawp_sidebar');
