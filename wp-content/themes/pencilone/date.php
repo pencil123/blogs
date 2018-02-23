f7c672a71e3bc873a6d5370dbaf16b5313f5aae5
grey <tianxiamall@163.com>
2018-02-23 17:29:01 +0800

    2-23

---
+++ wp-content/themes/pencilone/date.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * The main template file
+ * @Auther pencilone
+ * @version 1.0
+ */
+
+get_header(); ?>
+<div id="breadcrumb" class="wrapper">
+现在位置>首页
+</div>
+<div class="wrapper">
+<div id="content">
+<?php
+$year = get_query_var('year');
+$monthnum = get_query_var('monthnum');
+$day = get_query_var('day');
+
+if ($day) 
+    { $query_string = "year=".$year."&monthnum=".$monthnum."&day=".$day;}
+else
+    { $query_string = "year=".$year."&monthnum=".$monthnum;}
+
+$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+query_posts( $query_string.'&posts_per_page=10&paged='.$paged );
+while ( have_posts() ): the_post();?>
+<div class="item-list">
+<h2 class="post-box-title"><a href="<?php echo get_permalink(); ?>"><?php echo get_the_title();?></a></h2>
+<div class="post-meta">
+<span class="entry-date"><?php echo get_the_date('Y/m/d');?></span>
+<span class="entry-author"><?php echo get_the_author(); ?></span>
+<span class="update"><?php echo get_the_modified_time();?></span>
+</div>
+<div class="summary"><?php echo wp_trim_words( get_the_content(), 100 );?>
+</div>
+</div>
+<?php endwhile;?>
+
+<div class="pagenav"><?php native_pagenavi();?></div>
+
+</div>
+</div>
+
+<div id="sidebar">
+<?php dynamic_sidebar('rightsidebar'); ?>
+</div>
+
+<?php get_footer();
---
+++ wp-content/themes/pencilone/single.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * The main template file
+ * @Auther pencilone
+ * @version 1.0
+ */
+get_header(); ?>
+
+<div class="wrapper">
+    <main id="content">
+        <?php
+        // Start the loop.
+        while ( have_posts() ) : the_post();
+            /*
+             * Include the post format-specific template for the content. If you want to
+             * use this in a child theme, then include a file called content-___.php
+             * (where ___ is the post format) and that will be used instead.
+             */?>
+            <div class="post-box-title"><?php the_title();?></div>
+<div class="post-meta">
+<span class="entry-date"><?php echo get_the_date('Y/m/d');?></span>
+<span class="entry-author"><?php echo get_the_author(); ?></span>
+<span class="update"><?php echo get_the_modified_time();?></span>
+</div>
+            <div class="entry-content"><?php the_content();?></div>
+<?php 
+            // Previous/next post navigation.
+            the_post_navigation( array(
+                'next_text' => '%title',
+                'prev_text' => '%title',
+                'screen_reader_text' => 'Post navigation',
+            ) );
+
+        // End the loop.
+        endwhile;
+        ?>
+
+        </main>
+    </div><!-- .content-area -->
+<div id="sidebar">
+<?php dynamic_sidebar('rightsidebar'); ?>
+</div>
+
+<?php get_footer(); ?>
