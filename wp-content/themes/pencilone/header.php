ccaa016e6e6e3b0a5223a474dfd8793e20cd6b42
grey <tianxiamall@163.com>
2018-03-04 10:29:46 +0800

    2-3

--- wp-content/themes/pencilone/header.php
+++ wp-content/themes/pencilone/header.php
@@ -26,7 +26,11 @@
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
     <?php wp_head(); ?>
-    
+
+<meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+
+
 </head>
 
 <body <?php body_class(); ?>>
